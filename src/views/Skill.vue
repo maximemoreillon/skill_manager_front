7b1311d0432bbeaa0a9f585b8d8b4e566eb00c93
Maxime Moreillon <m.moreillon@gmail.com>
2020-07-02 02:46:36 +0000

    minor UI improvements

--- src/views/Skill.vue
+++ src/views/Skill.vue
@@ -4,18 +4,33 @@
     <h1 v-else>Skill</h1>
 
     <h2>Users with this skill</h2>
-    <div
-      class="employee"
-      v-for="user in sorted_users"
-      v-bind:key="user.employee.identity.low">
 
-      <span class="employee_name">{{user.employee.properties.display_name}}</span>
+    <div class="" v-if="loading">
+      Loading
+    </div>
+
+    <template v-else-if="users_with_this_skill.length > 0">
+      <div
+        class="employee"
+        v-for="user in sorted_users"
+        v-bind:key="user.employee.identity.low">
+
+        <router-link
+          class="employee_name"
+          :to="{ name: 'Home', query: {id: user.employee.identity.low} }">
+          {{user.employee.properties.display_name}}
+        </router-link>
 
 
-      <progress
-        v-bind:value="user.relationship.properties.profeciency"
-        min="0"
-        max="100"/>
+        <progress
+          v-bind:value="user.relationship.properties.profeciency"
+          min="0"
+          max="100"/>
+      </div>
+    </template>
+
+    <div class="" v-else>
+      No user with this skill
     </div>
 
   </div>
@@ -30,7 +45,8 @@ export default {
   data(){
     return {
       skill: null,
-      users_with_this_skill: []
+      users_with_this_skill: [],
+      loading: false,
     }
   },
   mounted(){
@@ -39,10 +55,12 @@ export default {
   methods: {
     get_skill(){
       if(this.$route.query.id){
+        this.loading = true
         let url = `${process.env.VUE_APP_SKILL_MANAGER_URL}/skills/${this.$route.query.id}/employees`
 
         this.axios.get(url)
         .then( (response) => {
+          if(!response.data.length) return
           let first_record = response.data[0]
           this.skill = first_record._fields[first_record._fieldLookup['skill']]
 
@@ -62,6 +80,7 @@ export default {
           if(error.response) console.log(error.response.data)
           alert(error)
         })
+        .finally(() => this.loading = false)
       }
     }
   },
